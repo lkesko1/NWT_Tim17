a95dfd7ae14403e718d437b0459ede02df34ac8a
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2018-03-18 00:08:25 +0100

    discovery server added

---
+++ nwt-eureka-server/src/main/java/ba/unsa/etf/nwteurekaserver/NwtEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.nwteurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwtEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwtEurekaServerApplication.class, args);
+	}
+}
