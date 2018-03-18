8300227f6dd27ddac34f900c676c5876104b0abb
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2018-03-18 13:23:23 +0100

    prev commit

---
+++ nwt-cinema-upcomings/src/main/java/ba/unsa/etf/nwtcinemaupcomings/NwtCinemaUpcomingsApplication.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.nwtcinemaupcomings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwtCinemaUpcomingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwtCinemaUpcomingsApplication.class, args);
+	}
+}
