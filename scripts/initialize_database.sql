a364a3d5fc6590fd1e2939d4299ee0b68e697af3
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2018-03-18 12:45:02 +0100

    sql script edit

--- scripts/initialize_database.sql
+++ scripts/initialize_database.sql
@@ -3,8 +3,10 @@ alter user nwt WITH ENCRYPTED PASSWORD 'nwt';
 
 CREATE DATABASE "nwt-cinema-auth";
 CREATE DATABASE "nwt-cinema-movies";
+CREATE DATABASE "nwt-cinema-projections";
+CREATE DATABASE "nwt-cinema-reservations";
 
 grant all PRIVILEGES on DATABASE "nwt-cinema-auth" to nwt;
 grant all PRIVILEGES on DATABASE "nwt-cinema-movies" to nwt;
-
-commit;
+grant all PRIVILEGES on DATABASE "nwt-cinema-projections" to nwt;
+grant all PRIVILEGES on DATABASE "nwt-cinema-reservations" to nwt;
