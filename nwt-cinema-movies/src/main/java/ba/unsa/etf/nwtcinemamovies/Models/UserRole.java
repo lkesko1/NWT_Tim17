1de1e727d757063d22ebba4a84fca39b3afce4d9
Edin Cerić <>
2018-03-22 22:59:34 +0100

    spring-models-implementation: Implemented spring models and added create and seeder script

---
+++ nwt-cinema-movies/src/main/java/ba/unsa/etf/nwtcinemamovies/Models/UserRole.java
@@ -0,0 +1,31 @@
+package ba.unsa.etf.nwtcinemamovies.Models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+
+@Entity
+public class UserRole {
+
+	@JoinColumn(name = "role_id")
+	private Integer roleId;
+
+	@Column(name = "user_id")
+	private Integer userId;
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(Integer roleId) {
+		this.roleId = roleId;
+	}
+}
