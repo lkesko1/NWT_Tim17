857c95634086c9b577a5b1ff076672ec01424fff
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2018-03-29 21:11:28 +0200

    services added to nwt-cinema-reservations

---
+++ nwt-cinema-reservations/src/main/java/ba/unsa/etf/nwtcinemareservations/services/BaseService.java
@@ -0,0 +1,24 @@
+package ba.unsa.etf.nwtcinemareservations.services;
+
+import ba.unsa.etf.nwtcinemareservations.models.AbstractModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public abstract class BaseService<M extends AbstractModel, R extends JpaRepository<M, Long>> {
+
+    @Autowired
+    private R repository;
+
+    public Iterable<M> findAll() {
+        return repository.findAll();
+    }
+    public M findById(Long id) {
+        return repository.getOne(id);
+    }
+    public M add(M m) {
+        return repository.save(m);
+    }
+    public M update(M m) {
+        return repository.save(m);
+    }
+}
---
+++ nwt-cinema-reservations/src/main/java/ba/unsa/etf/nwtcinemareservations/services/ReservationService.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.nwtcinemareservations.services;
+
+import ba.unsa.etf.nwtcinemareservations.models.Reservation;
+import ba.unsa.etf.nwtcinemareservations.repositories.IReservationRepository;
+
+public class ReservationService extends BaseService<Reservation, IReservationRepository> {
+}
+
