4241c153a3249444dd5030c0709d9937d7a3c963
Adnan Alibegovic <aalibegovi2@etf.unsa.ba>
2018-03-29 22:36:08 +0200

    minor fixes

--- nwt-cinema-reservations/src/main/java/ba/unsa/etf/nwtcinemareservations/controllers/BaseController.java
+++ nwt-cinema-reservations/src/main/java/ba/unsa/etf/nwtcinemareservations/controllers/BaseController.java
@@ -5,19 +5,21 @@ import ba.unsa.etf.nwtcinemareservations.services.BaseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.transaction.Transactional;
-import java.util.List;
 
 public abstract class BaseController<S extends BaseService> {
     protected Logger logger = LoggerFactory.getLogger(getClass());
 
-    protected S service;
+    private S service;
+
+    public S getService() {
+        return this.service;
+    }
 
     @Autowired
     public void setService(S service) {
@@ -26,8 +28,8 @@ public abstract class BaseController<S extends BaseService> {
 
     @Transactional
     @RequestMapping(value = "create", method = RequestMethod.POST)
-    public void create(@RequestBody final AbstractModel model) {
-        service.add(model);
+    public AbstractModel create(@RequestBody final AbstractModel model) {
+        return service.add(model);
     }
 
 
@@ -38,7 +40,7 @@ public abstract class BaseController<S extends BaseService> {
     }
 
     @Transactional
-    @RequestMapping(value = "{reviewId}", method = RequestMethod.GET)
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
     public AbstractModel findById(@PathVariable("id") final Long id) {
         return service.findById(id);
     }
