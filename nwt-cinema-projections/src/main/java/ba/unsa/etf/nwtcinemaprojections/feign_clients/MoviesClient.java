2aec244e2dc80757137d7ccf33f521f8e540362f
Anisa Hadzibulic <ahadzibuli1@etf.unsa.ba>
2018-04-13 13:08:47 +0200

    first version - done

--- nwt-cinema-projections/src/main/java/ba/unsa/etf/nwtcinemaprojections/feign_clients/MoviesClient.java
+++ nwt-cinema-projections/src/main/java/ba/unsa/etf/nwtcinemaprojections/feign_clients/MoviesClient.java
@@ -1,9 +1,18 @@
 package ba.unsa.etf.nwtcinemaprojections.feign_clients;
 
+import ba.unsa.etf.nwtcinemaprojections.feign_clients.dto.MovieDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
 @FeignClient("nwt-cinema-movies")
 public interface MoviesClient {
+
+    @RequestMapping(method = RequestMethod.GET, value = "movies/movies/{id}")
+    MovieDTO getMovieDetails(@PathVariable("id") Long id);
+
+
 }
--- nwt-cinema-projections/src/main/java/ba/unsa/etf/nwtcinemaprojections/services/MovieTimetableService.java
+++ nwt-cinema-projections/src/main/java/ba/unsa/etf/nwtcinemaprojections/services/MovieTimetableService.java
@@ -1,14 +1,20 @@
 package ba.unsa.etf.nwtcinemaprojections.services;
 
 import ba.unsa.etf.nwtcinemaprojections.controllers.dto.TicketReservationDTO;
+import ba.unsa.etf.nwtcinemaprojections.feign_clients.MoviesClient;
+import ba.unsa.etf.nwtcinemaprojections.feign_clients.dto.MovieDTO;
 import ba.unsa.etf.nwtcinemaprojections.models.MovieTimetable;
 import ba.unsa.etf.nwtcinemaprojections.repositories.IMovieTimetableRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class MovieTimetableService extends BaseService<MovieTimetable, IMovieTimetableRepository>{
+
+    @Autowired
+    private MoviesClient moviesClient;
 
     public void reserveTickets(TicketReservationDTO ticketReservation) throws Exception {
         Optional<MovieTimetable> timetable = this.repository.findById(ticketReservation.getProjectionId());
@@ -18,6 +24,11 @@ public class MovieTimetableService extends BaseService<MovieTimetable, IMovieTim
         }
         timetable.get().setActualTickets(newNumberOfTickets);
         this.repository.save(timetable.get());
+    }
+
+    public MovieDTO getDetails(Long movieId){
+        return moviesClient.getMovieDetails(movieId);
+
     }
 
 
