9bb5d7c7076f999ab948e67c69ee44d51a42da94
Lejla Kesko <lejla.kesko@hotmail.com>
2018-05-13 19:09:02 +0200

    create gateway container

--- nwt-api-gateway/src/main/java/ba/unsa/etf/nwtapigateway/NwtApiGatewayApplication.java
+++ nwt-api-gateway/src/main/java/ba/unsa/etf/nwtapigateway/NwtApiGatewayApplication.java
@@ -3,9 +3,11 @@ package ba.unsa.etf.nwtapigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableZuulProxy
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class NwtApiGatewayApplication {
 
 	public static void main(String[] args) {
